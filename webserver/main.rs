386364fe7ba01891b663dc26050297f8384b9179
SebastianMeisel <sebastian.meisel@gmail.com>
2025-08-02 15:22:54 +0200

    UI changes

--- webserver/main.rs
+++ webserver/main.rs
@@ -15,14 +15,45 @@ fn main() {
                 }
                 let mut path = std::path::PathBuf::new();
                 path.push("htdocs");
-                path.push(resource.trim_start_matches("/"));
+		let index = resource.find('?').unwrap_or(resource.len());
+                path.push(resource[0..index].trim_start_matches("/"));
                 if resource.ends_with("/") { path.push("index.html"); }
                 stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
                 stream.write_all(b"Server: Computerphile Rust\r\n").unwrap();
-                stream.write_all(b"Content-type: text/html\r\n\r\n").unwrap();
+                stream.write_all(b"Content-type: text/html ; charset=utf-8\r\n\r\n").unwrap();
                 stream.write_all(&std::fs::read(path).unwrap()).unwrap();
             }
+            ["POST", _resource, "HTTP/1.1"] => {
+                loop {
+                    let mut line = String::new();
+                    rdr.read_line(&mut line).unwrap();
+	       	    println!("{}",line);
+                    if line.trim().is_empty() { break; }
+                }
+                stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
+                stream.write_all(b"Server: Computerphile Rust\r\n").unwrap();
+                stream.write_all(b"Content-type: text/html ; charset=utf-8\r\n\r\n").unwrap();
+                stream.write_all(b"<!DOCTYPE html>\r\n").unwrap();
+                stream.write_all(b"<html><body><p>You wrote:\r\n").unwrap();
+                stream.write_all(b"</p></body></html>\r\n").unwrap();
+            }
+	    ["inputString=",answer] => {
+                loop {
+                    let mut line = String::new();
+                    rdr.read_line(&mut line).unwrap();
+	       	    println!("{}",line);
+                    if line.trim().is_empty() { break; }
+                }
+	    	stream.write_all(answer.as_bytes()).unwrap();
+                stream.write_all(b"</p></body></html>\r\n").unwrap();
+            }
             _ => {
+                loop {
+                    let mut line = String::new();
+                    rdr.read_line(&mut line).unwrap();
+	       	    println!("-{}",line);
+                    if line.trim().is_empty() { break; }
+                }
                 stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
             }
         }
